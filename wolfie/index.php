acd05a48fb2a6d6eb40de747850303a0cbc24e16
Silvio Mayolo <mercerenies@comcast.net>
2022-06-26 18:05:52 -0400

    Prod version of wolfie receiver

---
+++ wolfie/index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Wolfie the Werewolf Bot</title>
+  </head>
+  <body>
+    <p>
+      This is the web service which hosts game logs for my One-Night
+      Werewolf Discord bot, affectionately known as Wolfie.
+    </p>
+    <p>
+      For more information, visit <a href="https://github.com/Mercerenies/werewolf-bot">the project's GitHub page</a>
+    </p>
+  </body>
+</html>
